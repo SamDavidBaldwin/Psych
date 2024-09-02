9cf18e4fed961e0445023a51e55aa8ff0a82de4c
Samuel Baldwin <sdb035@bucknell.edu>
2024-09-02 15:21:06 -0400

    Adjusting Structure for SB

---
+++ src/main/java/com/psych/Brief2/Brief2Application.java
@@ -0,0 +1,13 @@
+package com.psych.Brief2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Brief2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Brief2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/psych/Brief2/Brief2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psych.Brief2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Brief2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
